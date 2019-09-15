7e5d3084a0d11f08373474303f0fab7f8c84b939
Alexandre Justino <alexandretok@gmail.com>
2019-09-15 17:03:49 +0100

    Add event binding, enum, string interpolation

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,8 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+enum ItemStatus {
+  TODO,
+  DONE
+}
+
 interface Item {
   label: string;
-  value: string;
+  value: number;
+  status: ItemStatus
 };
 
 @Component({
@@ -12,18 +18,31 @@ interface Item {
 })
 export class TodoListComponent implements OnInit {
   items: Item[];
+  listTitle: string = "Project Tasks";
 
   constructor() {}
+
+  addItem() {
+    const newItem: Item = {
+      label: `Item #${this.items.length + 1}`,
+      value: this.items.length + 1,
+      status: ItemStatus.TODO
+    }
+
+    this.items.push(newItem);
+  }
 
   ngOnInit() {
     this.items = [
       {
         label: "Have breakfest",
-        value: "hv_bf"
+        value: 1,
+        status: ItemStatus.TODO
       },
       {
         label: "Make coffee",
-        value: "mk_cf"
+        value: 2,
+        status: ItemStatus.TODO
       }
     ];
   }
